c2116ee3fd64646a9948b41903d3a1a0e7a548cb
Jacob Hickman <strivelinkband@yahoo.com>
2021-10-25 02:32:57 -0500

    Updated Homepage Portrait and added styles

--- src/layout/home/Home.js
+++ src/layout/home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import portrait from '../../imgs/Portrait.jpg';
+import portrait from '../../imgs/PortraitCropped.jpg';
 import backgroundImage from '../../imgs/BackgroundImage.jpg';
 
 import './home.css'
@@ -11,16 +11,18 @@ export default function Home() {
     <div className='home-bg'>
       <div className='intro-card'>
         <div className='card card-background intro'>
-          <img src={portrait} alt='Dakota J. Edwards' className='portrait' />
+          <div className='img-container'>
+            <img src={portrait} alt='Dakota J. Edwards' className='portrait' />
+          </div>
           <div className='introduction-text'>
             <h3>Introduction</h3>
             <p>Back in 2018, I thought I would end up as a pilot.
-              Next thing I know, I have a camera in my hand and I'm spending my weekends shooting Music Videos,
-              Ads, and Weddings. My goal is to scale my business, Hungry & Humble, from a solo operation into a
-              full production company aimed at helping musicians create great content to grow their brands and share
+            Next thing I know, I have a camera in my hand and I'm spending my weekends shooting Music Videos,
+            Ads, and Weddings. My goal is to scale my business, Hungry & Humble, from a solo operation into a
+            full production company aimed at helping musicians create great content to grow their brands and share
               their messages with the world.</p>
             <p>I've had the privilege of producing, directing, shooting, and editing several projects within the
-              Provo/SLC Music Scene over the last two years, and I'm looking forward with excitement towards the
+            Provo/SLC Music Scene over the last two years, and I'm looking forward with excitement towards the
               many projects to come this year!</p>
           </div>
         </div>
