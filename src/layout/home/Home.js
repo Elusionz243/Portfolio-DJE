92f0602bb4cede56d9c5f18847167e8b1b7ef275
Elusionz243 <strivelinkband@yahoo.com>
2021-10-25 19:12:37 -0500

    Added styles to Homepage

--- src/layout/home/Home.js
+++ src/layout/home/Home.js
@@ -15,15 +15,19 @@ export default function Home() {
             <img src={portrait} alt='Dakota J. Edwards' className='portrait' />
           </div>
           <div className='introduction-text'>
-            <h3>Introduction</h3>
-            <p>Back in 2018, I thought I would end up as a pilot.
-            Next thing I know, I have a camera in my hand and I'm spending my weekends shooting Music Videos,
-            Ads, and Weddings. My goal is to scale my business, Hungry & Humble, from a solo operation into a
-            full production company aimed at helping musicians create great content to grow their brands and share
-              their messages with the world.</p>
-            <p>I've had the privilege of producing, directing, shooting, and editing several projects within the
-            Provo/SLC Music Scene over the last two years, and I'm looking forward with excitement towards the
-              many projects to come this year!</p>
+            <h3 style={{textDecoration: 'underline'}}><strong>Introduction</strong></h3>
+            <p>
+              Back in 2018, I thought I would end up as a pilot.
+              Next thing I know, I have a camera in my hand and I'm spending my weekends shooting Music Videos,
+              Ads, and Weddings. My goal is to scale my business, Hungry & Humble, from a solo operation into a
+              full production company aimed at helping musicians create great content to grow their brands and share
+              their messages with the world.
+            </p>
+            <p>
+              I've had the privilege of producing, directing, shooting, and editing several projects within the
+              Provo/SLC Music Scene over the last two years, and I'm looking forward with excitement towards the
+              many projects to come this year!
+            </p>
           </div>
         </div>
       </div>
