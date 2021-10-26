e8127358d7a5693b04cb397808b40bfac415dc4d
Elusionz243 <strivelinkband@yahoo.com>
2021-10-26 00:04:53 -0500

    Added @Media styles for home page, Home page is now Mobile Responsive

--- src/common/NavigationBar.js
+++ src/common/NavigationBar.js
@@ -44,21 +44,33 @@ export default function NavigationBar() {
               <a href='/contact' className='navbar__links'>
                 <i className='bi bi-telephone-fill'></i>
                 Contact
-                </a>
+              </a>
             </li>
           </ul>
           : <ul className='navbar__menu active'>
             <li className='navbar__item'>
-              <a href="/" className='navbar__links'>Home</a>
+              <a href="/" className='navbar__links'>
+                <i className='bi bi-house-door-fill'></i>
+                Home
+              </a>
             </li>
             <li className='navbar__item'>
-              <a href='/about' className='navbar__links'>About</a>
+              <a href='/about' className='navbar__links'>
+                <i className='bi bi-file-person'></i>
+                About
+              </a>
             </li>
             <li className='navbar__item'>
-              <a href='/projects' className='navbar__links'>Projects</a>
+              <a href='/projects' className='navbar__links'>
+                <i className='bi bi-camera-reels-fill'></i>
+                Projects
+              </a>
             </li>
             <li className='navbar__item'>
-              <a href='/contact' className='navbar__links'>Contact</a>
+              <a href='/contact' className='navbar__links'>
+                <i className='bi bi-telephone-fill'></i>
+                Contact
+              </a>
             </li>
           </ul>}
       </div>
