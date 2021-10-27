ccd30e80c241988e12ac92c105361d5443058df0
Elusionz243 <strivelinkband@yahoo.com>
2021-10-27 01:53:04 -0500

    Finished Styling for About page. Added images and unupdated text. About page now mobile responsive

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Switch, Route } from 'react-router-dom';
 import NavigationBar from './common/NavigationBar';
+import About from './layout/about/About';
 import Home from './layout/home/Home';
 
 export default function Routes() {
@@ -10,6 +11,9 @@ export default function Routes() {
       <Switch>
         <Route path='/' exact={true}>
           <Home />
+        </Route>
+        <Route path='/about'>
+          <About />
         </Route>
       </Switch>
     </div>
---
+++ src/layout/about/About.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import './about.css';
+
+import portrait from '../../imgs/AboutPortrait.jpg';
+import directorSet from '../../imgs/DirectorSet.jpg';
+
+export default function About() {
+  return (
+    <div className='about-container'>
+      <div className='card about-card'>
+        <div className='about-inner-card'>
+          <div className='about-section-1'>
+            <img src={portrait} alt='Dakota J. Edwards' className='about-portrait' />
+            <p className='about-text'>
+              Throughout my life, I’ve been exposed to a variety of movies and performances,
+              which inspired me to pursue a career in the film industry. My work ethic and
+              love for the visual arts motivates me as a student, and pushes me to constantly
+              develop my storytelling voice.
+            </p>
+          </div>
+          <div className='about-section-2'>
+            <p className='about-text'>
+              I’m a curious and innovative film school student, and am currently working on some
+              fascinating projects. Get in touch to learn more about my work, or if you’d be interested
+              in collaborating together in the future.
+            </p>
+            <img src={directorSet} alt='Director Set' className='director-set-image' />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
