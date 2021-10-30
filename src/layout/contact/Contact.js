823d2a9d91e522489d78d2b30ee305f1a18d25fa
Elusionz243 <strivelinkband@yahoo.com>
2021-10-29 20:06:02 -0500

    Finished Contact Page with styling (Mobile Responsive). Waiting for Email to finish Sumbitting process.

--- src/layout/contact/Contact.js
+++ src/layout/contact/Contact.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './contact.css';
 
 export default function Contact() {
+  const [fade, setFade] = useState('');
+  useEffect(() => {
+    setFade(c => c += 'fade-in');
+  }, [setFade]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
   }
 
   return (
-    <div className='contact-background'>
-      <div className='card contact-card'>
-        <div className='contact-form'>
-          {/* <form onSubmit={handleSubmit}> */}
-            <fieldset>
-              <input type='text' id='email' />
-            </fieldset>
-          {/* </form> */}
-        </div>
+    <div className={`contact-background ${fade}`}>
+      <div className='contact-card'>
+        <u className='contact-title'><strong>Contact Me</strong></u>
+        <form onSubmit={handleSubmit} className='contact-form'>
+          <fieldset className='mb-3'>
+            <label htmlFor='name' className='form-label'>Name</label>
+            <input type='text' id='name' className='form-control' placeholder='John Doe' />
+          </fieldset>
+          <fieldset className='mb-3'>
+            <label htmlFor='email' className='form-label'>Email address</label>
+            <input type='text' id='email' className='form-control' placeholder='example@email.com' />
+          </fieldset>
+          <fieldset className='mb-3'>
+            <label htmlFor='message' className='form-label'>Message</label>
+            <textarea className='form-control' id='message' rows='6' placeholder={`Enter a message`}></textarea>
+          </fieldset>
+          <button type='submit' className='btn btn-primary submit-button' disabled>Submit</button>
+        </form>
       </div >
     </div >
   );
